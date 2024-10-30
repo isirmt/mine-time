7687e155db4f41f01e1de79c20a54014f7f5c858
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-10-30 11:31:29 +0900

    test: pusherライブラリ、メッセージ送信テスト・描画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import MessageList from "@/components/MessageList";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl font-bold">Home</h1>
-      <div>
-        <span className="text-blue-500">Hello World!</span>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MessageList />
     </main>
   );
 }
